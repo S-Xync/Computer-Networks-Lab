2fa3346ddb939ee88d2f425a9fe47f5e61cd9128
SaiKumar Immadi <immadisaikumar@ymail.com>
2017-01-09 11:07:52 +0530

    Creates ser.c and cli.c in lab1 for my own implementaions

--- lab1/clie2.c
+++ lab1/clie2.c
@@ -17,39 +17,39 @@ int main(void)
 
   memset(recvBuff, '0' ,sizeof(recvBuff));
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
-    {
-      printf("\n Error : Could not create socket \n");
-      return 1;
-    }
+  {
+    printf("\n Error : Could not create socket \n");
+    return 1;
+  }
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(5000);
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
-    {
-      printf("\n Error : Connect Failed \n");
-      return 1;
-    }
+  {
+    printf("\n Error : Connect Failed \n");
+    return 1;
+  }
 
   while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
-      if(n<0){
-        printf("Error receiveing from server.\n");
-      }
-      recvBuff[n] = 0;
-      printf("%s",recvBuff);
+  {
+    if(n<0){
+      printf("Error receiveing from server.\n");
+    }
+    recvBuff[n] = 0;
+    printf("%s",recvBuff);
     //   if(fputs(recvBuff, stdout) == EOF)
     // {
     //   printf("\n Error : Fputs error");
     // }
-      printf("\n");
-    }
+    printf("\n");
+  }
 
   if( n < 0)
-    {
-      printf("\n Read Error \n");
-    }
+  {
+    printf("\n Read Error \n");
+  }
 
   return 0;
 }
---
+++ lab1/ser.c
@@ -0,0 +1,10 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <errno.h>

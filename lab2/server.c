13704a9e133f1518631cf4ed032175ee542d3e16
SaiKumar Immadi <immadisaikumar@ymail.com>
2017-01-16 10:18:14 +0530

    A working multithreaded server of lab1 done in lab2

--- lab2/server.c
+++ lab2/server.c
@@ -10,21 +10,25 @@
 #include <errno.h>
 #include <time.h>//for time usage
 #include <ctype.h>//for lower upper case usage
+#include <pthread.h> // for threading
+
+void *connection_handler(void *);//function that handles each thread
 
 int main(int argc, char const *argv[]) {
-  int listening_sock=0,connection=0;
-  int n=0;
-  struct sockaddr_in server_address;
-  char sending_buffer[1024];
-  char receiving_buffer[1024];
-  time_t time_now;
+  int listening_sock=0,client_sock=0,c;
+  // int connection=0;
+  // int n=0;
+  struct sockaddr_in server_address,client_address;
+  // char sending_buffer[1024];
+  // char receiving_buffer[1024];
+  // time_t time_now;
   if((listening_sock = socket(AF_INET, SOCK_STREAM, 0))< 0){
     printf("\n Error : Could not create listening socket \n");
     return 1;
   }
   printf("Listening socket created successfully\n");
-  memset(sending_buffer,'0',sizeof(sending_buffer));
-  memset(receiving_buffer,'0',sizeof(receiving_buffer));
+  // memset(sending_buffer,'0',sizeof(sending_buffer));
+  // memset(receiving_buffer,'0',sizeof(receiving_buffer));
   server_address.sin_family=AF_INET;
   server_address.sin_addr.s_addr=htonl(INADDR_ANY);
   server_address.sin_port=htons(5432);
@@ -35,37 +39,93 @@ int main(int argc, char const *argv[]) {
   }
   printf("Running!!\n");
   printf("Waiting for connections!!\n");
-  int count=0;
-  while (1) {
-    connection=accept(listening_sock,(struct sockaddr*)NULL,NULL);
-    if(connection){
-      count++;
-      printf("\nConnected to a client No : %d\n",count);
+  c = sizeof(struct sockaddr_in);
+  pthread_t thread_id;
+  while((client_sock = accept(listening_sock, (struct sockaddr *)&client_address, (socklen_t*)&c))) {
+    puts("Connection accepted");
+    if(pthread_create( &thread_id, NULL,  connection_handler, (void*) &client_sock) < 0) {
+      perror("could not create thread");
+      return 1;
     }
-    time_now=time(NULL);
-    snprintf(sending_buffer,sizeof(sending_buffer),"Time --> %.24s\r\n",ctime(&time_now));
-    write(connection,sending_buffer,strlen(sending_buffer));
-    while((n=read(connection,receiving_buffer,sizeof(receiving_buffer)-1))>0){
-      if(n<0){
-        printf("Error receiving from client");
-      }
-      receiving_buffer[n]=0;
-      printf("client : %s",receiving_buffer);
-      write(connection,receiving_buffer,strlen(receiving_buffer));
-      for(int i=0;i<n;i++){
-        receiving_buffer[i]=tolower(receiving_buffer[i]);
-      }
-      char subbuff[5];
-      memcpy( subbuff, &receiving_buffer[0], 3 );
-      subbuff[3] = '\0';
-      if(strcmp(subbuff,"bye")==0){
-        printf("\nClient Sent Bye. Dropped Client No : %d\n\n",count);
-        printf("Waiting for connections!!\n");
-        strcpy(sending_buffer,"Sent Bye!! Dropping Connection.\nBye!!\n");
-        write(connection,sending_buffer,strlen(sending_buffer));
-        close(connection);
-      }
-    }
+    // Now join the thread, so that we dont terminate before the thread
+    // pthread_join( thread_id, NULL);
+    puts("Handler assigned");
+  }
+  if (client_sock < 0) {
+    perror("accept failed");
+    return 1;
   }
   return 0;
 }
+
+
+void* connection_handler(void* socket_desc){
+  int connection =*(int*)socket_desc;
+  char sending_buffer[1024];
+  char receiving_buffer[1024];
+  int n=0;
+  time_t time_now;
+  memset(sending_buffer,'0',sizeof(sending_buffer));
+  memset(receiving_buffer,'0',sizeof(receiving_buffer));
+  if(connection){
+    printf("\nConnected to a client\n");
+  }
+  time_now=time(NULL);
+  snprintf(sending_buffer,sizeof(sending_buffer),"Time --> %.24s\r\n",ctime(&time_now));
+  write(connection,sending_buffer,strlen(sending_buffer));
+  while((n=read(connection,receiving_buffer,sizeof(receiving_buffer)-1))>0){
+    if(n<0){
+      printf("Error receiving from client");
+    }
+    receiving_buffer[n]=0;
+    printf("client : %s",receiving_buffer);
+    write(connection,receiving_buffer,strlen(receiving_buffer));
+    for(int i=0;i<n;i++){
+      receiving_buffer[i]=tolower(receiving_buffer[i]);
+    }
+    char subbuff[5];
+    memcpy( subbuff, &receiving_buffer[0], 3 );
+    subbuff[3] = '\0';
+    if(strcmp(subbuff,"bye")==0){
+      printf("\nClient Sent Bye. Dropped Client\n\n");
+      printf("Waiting for connections!!\n");
+      strcpy(sending_buffer,"Sent Bye!! Dropping Connection.\nBye!!\n");
+      write(connection,sending_buffer,strlen(sending_buffer));
+      close(connection);
+    }
+  }
+return 0;
+}
+
+  // while (1) {
+    // connection=accept(listening_sock,(struct sockaddr*)NULL,NULL);
+    // if(connection){
+      // printf("\nConnected to a client\n");
+    // }
+    // time_now=time(NULL);
+    // snprintf(sending_buffer,sizeof(sending_buffer),"Time --> %.24s\r\n",ctime(&time_now));
+    // write(connection,sending_buffer,strlen(sending_buffer));
+  //   while((n=read(connection,receiving_buffer,sizeof(receiving_buffer)-1))>0){
+  //     if(n<0){
+  //       printf("Error receiving from client");
+  //     }
+  //     receiving_buffer[n]=0;
+  //     printf("client : %s",receiving_buffer);
+  //     write(connection,receiving_buffer,strlen(receiving_buffer));
+  //     for(int i=0;i<n;i++){
+  //       receiving_buffer[i]=tolower(receiving_buffer[i]);
+  //     }
+  //     char subbuff[5];
+  //     memcpy( subbuff, &receiving_buffer[0], 3 );
+  //     subbuff[3] = '\0';
+  //     if(strcmp(subbuff,"bye")==0){
+  //       printf("\nClient Sent Bye. Dropped Client No : %d\n\n",count);
+  //       printf("Waiting for connections!!\n");
+  //       strcpy(sending_buffer,"Sent Bye!! Dropping Connection.\nBye!!\n");
+  //       write(connection,sending_buffer,strlen(sending_buffer));
+  //       close(connection);
+  //     }
+  //   }
+  // }
+  // return 0;
+// }
